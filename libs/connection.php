36b0e689861d5fe4d93d18529812b001ecdadfb9
Nenet <jannet_butuc@yahoo.fr>
2015-09-09 21:16:25 +0200

    connexions
    connexion to the file : enregistrement

--- libs/connection.php
+++ libs/connection.php
@@ -1,7 +1,18 @@
 <?php
+// Connexion 
 // Création de l'objet PDO et connexion au serveur MySQL
 try {
     $monObjet = new PDO("mysql:host=localhost;dbname=urban", 'root', '',
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $monObjet->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur'.$e->getMessage());
+}
+
+// Enregistrement 
+// Création de l'objet PDO et connexion au serveur MySQL
+try {
+    $monObjet = new PDO("mysql:host=localhost;dbname=enregistrement", 'root', '',
             array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $monObjet->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
